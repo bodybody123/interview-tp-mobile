6b25e0e4ac197fb27b896119d0407ed4ed334854
bodybody123 <bodybodyofficial@gmail.com>
2025-02-24 14:12:59 +0700

    change from legacy input manager to input system for better cross compatibility

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
@@ -6,12 +7,17 @@ public class InputManager : MonoBehaviour
     private Camera sceneCamera;
     [SerializeField]
     private LayerMask placementLayerMask;
-    
     private Vector3 lastPosition;
 
+    private PlayerInput playerInput;
+    void Awake() {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
     public Vector3 GetSelectedMapPosition() {
+        Vector3 pos = playerInput.actions["Position"].ReadValue<Vector2>();
         Vector3 mousePos = Input.mousePosition;
-        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        Ray ray = sceneCamera.ScreenPointToRay(pos);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, 100, placementLayerMask)) {
